530bb22a6b07592a5b39d30648557638fd69a256
JuniorSierra <juniorsierramorales2002@gmailcom>
2024-03-25 14:07:34 -0500

    Agregue estilos y las funciones correspondiente de cada boton(NO tienen funcialidades)

--- script.js
+++ script.js
@@ -5,4 +5,31 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
-const button1 = document.querySelector("#button1");
+
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+// Inicializar botones, para que cuando les den click se ejecute la funcion correspondiente.
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+function goStore() {
+    console.log("Yendo a la tienda.");
+  }
+  
+  function goCave() {
+    console.log("Yendo a la cueva.");
+  }
+  
+  function fightDragon() {
+    console.log("Luchar contra dragones");
+  }
